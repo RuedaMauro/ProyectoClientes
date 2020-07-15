cde86529c980672f51f69cdac7c3727f0b239d93
unknown <mau9lr9@gmail.com>
2020-07-15 18:09:59 -0300

    ProyectoAlta

---
+++ ProyectoClientes.js
@@ -0,0 +1,8 @@
+var nombreCliente;
+var apellidoCliente;
+
+nombreCliente = prompt("Porfavor ingrese su nombre de usuario: ");
+apellidoCliente = propmt("Porfavor ingrese su apellido: ");
+
+alert("Bienvenido" + nombreCliente + " " + apellidoCliente);
+
